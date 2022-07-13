cbe45d74df72568c8454a331c8c91b0a6e3c8761
asrarbutt <asrarbutt1986@gmail.com>
2022-07-13 21:33:38 +0200

    amend WelcomeController, finish task

--- src/main/java/com/example/restcontrollerget/controller/WelcomeController.java
+++ src/main/java/com/example/restcontrollerget/controller/WelcomeController.java
@@ -1,4 +1,28 @@
 package com.example.restcontrollerget.controller;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/welcome")
 public class WelcomeController {
+
+    String name="";
+    @GetMapping
+    public String welcomeMessage(){
+        return "welcome "+name;
+    }
+
+    @GetMapping(path="/{name}")
+    public String welcomeUser(@PathVariable String name, @RequestParam String q){
+
+        return "your username is: "+name + " "+q;
+    }
+    @PostMapping("/sendname")
+    public String sendPostRequest(@RequestBody String inputName){
+        this.name=inputName;
+       int nameLenght=name.length();
+       String i=String.valueOf(nameLenght);
+        return "der name: " + name + "hat eine länge von: "+ i ;
+
+    }
 }
